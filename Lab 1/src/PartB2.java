165a5a1c1674a662700aced9d737c8da7d9810c5
mikeycanoe <mikanian17@gmail.com>
2015-01-14 15:29:05 -0700

    the working edition of part B2.

--- Lab 1/src/PartB2.java
+++ Lab 1/src/PartB2.java
@@ -2,15 +2,13 @@ import java.util.Scanner;
 public class PartB2 {
 
 	static Scanner input = new Scanner(System.in);
+	static String What_Is = ("What is your taxable income?");
+	static String Taxes = ("Your tax is $");
 	
 	public static void main(String[] args) {
 		System.out.println("What is your filing status? Single, Married "
-<<<<<<< HEAD
 				+ "filing jointly (qualifying widow(er)), \nMarried filing separately"
-				+ " or Head of household?");
-=======
-				+ "filing jointly (qualifying widow(er), or Married filing separately?");
->>>>>>> fb27aa43dfdf19c970ae0cdc7abff44ba771c090
+				+ ", or Head of household?");
 		String user = input.nextLine();
 		
 		if (user.equalsIgnoreCase("single")){
@@ -24,40 +22,78 @@ public class PartB2 {
 		if(user.equalsIgnoreCase("Married filing separately")){
 			MarriedSeparate();
 		}
-<<<<<<< HEAD
 		
 		if(user.equalsIgnoreCase("Head of household")){
 			HeadOf();
 		}
-=======
->>>>>>> fb27aa43dfdf19c970ae0cdc7abff44ba771c090
 	}
 	
 	public static void Single(){
-		System.out.println("What is your taxable income?");
-		int income = input.nextInt();
-<<<<<<< HEAD
+		System.out.println(What_Is);
+		float income = input.nextInt();
 		
-=======
->>>>>>> fb27aa43dfdf19c970ae0cdc7abff44ba771c090
+		if(income > 36250 && income <= 87850){
+			int tax = (int)(4991.25 + (((income + 25) - 36250) * .25));
+			System.out.println(Taxes + tax);
+		}
+		
+		if(income > 87850 && income <= 183250){
+			if(income >= 100000){
+				int tax = (int)((income * .28) - 6706.75);
+				System.out.println(Taxes + tax);
+			}
+			if(income < 100000){
+				int tax =(int) ((17891.25 + ((income - 87850) * .28) + 6.75));
+				System.out.println(Taxes + tax);
+			}
+		}
 	}
 	
 	public static void MarriedJointly(){
-		System.out.println("What is your taxable income?");
+		System.out.println(What_Is);
 		int income = input.nextInt();
+		
+		if(income > 72500 && income <= 146400){
+			if(income >= 100000){
+				double tax = (income * .25) - 8142.50;
+				System.out.println(Taxes + tax);
+			}
+			if(income < 100000){
+				int tax = (int) ((9982.5 + (((income + 25) - 72500) * .25)) + .25);
+				System.out.println(Taxes + tax);
+			}
+		}
 	}
 	
 	public static void MarriedSeparate(){
-		System.out.println("What is your taxable income?");
+		System.out.println(What_Is);
 		int income = input.nextInt();
+		
+		if(income > 73200 && income <= 111525){
+			if(income >= 100000){
+				double tax = (income * .28) - 6267.25;
+				System.out.println(Taxes + tax);
+			}
+			if(income < 100000){
+				int tax = (int) ((14228.75 + (((income + 25) - 73200) * .28)) + .25);
+				System.out.println(Taxes + tax);
+			}
+		}
 	}
-<<<<<<< HEAD
 	
 	public static void HeadOf(){
-		System.out.println("What is your taxable income?");
+		System.out.println(What_Is);
 		int income = input.nextInt();
+		
+		if(income > 48600 && income <= 125450){
+			if(income >= 100000){
+				double tax = (income * .25) - 5497.50;
+				System.out.println(Taxes + tax);
+			}
+			if(income < 100000){
+				int tax = (int) ((6652.50 + (((income + 25) - 48600) * .25)) + .25);
+				System.out.println(Taxes + tax);
+			}
+		}
 	}
-=======
->>>>>>> fb27aa43dfdf19c970ae0cdc7abff44ba771c090
-
 }
